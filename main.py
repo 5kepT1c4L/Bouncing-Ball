6333888ba6c5ee7dd7308b780fd568ca0431630b
Eric Jing <70067413+5kepT1c4L@users.noreply.github.com>
2021-08-23 11:38:31 -0400

    Finished Paddle

--- main.py
+++ main.py
@@ -1,63 +1,79 @@
 from tkinter import *
 import random
 import time
-
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-
-        #Direction randomizer
-        starts = [-3, -2, -2, 1, 2, 3]
+        starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
         self.x = starts[0]
         self.y = -3
-
-        #Ensure ball bounces off sides of screen
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
 
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
-
-        if pos[1] <= 0:     #Check to see if the ball touches the ceiling
-            self.y = 1      #If so, start bouncing down
-
-        if pos[3] >= self.canvas_height:    #Check to see if the ball touches the bottom of screen
-            self.y = -1     #If so, start bouncing up
-
-        if pos[0] <= 0:     #Check to see if ball touches left sides
-            self.x = 3      #If so, start bouncing right
-
-        if pos[2] >= self.canvas_width:     #Check to see if ball touches right side
-            self.x = -3     #If so, start boucning left
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
 
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
-
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+    def turn_left(self, evt):
+        self.x = -2
+    def turn_right(self, evt):
+        self.x = 2
+
 tk = Tk()
-tk.title("Pong Game")
+tk.title("Game")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-ball = Ball(canvas, 'red')
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
 
 while 1:
-    ball.draw()
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(0.01)
+    if ball.hit_bottom == False:
+        ball.draw()
+        paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
 
 
